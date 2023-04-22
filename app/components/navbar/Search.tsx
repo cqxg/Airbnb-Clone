20813e299aaf565fa1ece427392b7cb5425add77
cqxg <ihar.o.ivaniuk@gmail.com>
2023-04-22 22:51:30 +0200

    feat: add search refinement

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,18 +1,20 @@
 "use client";
 
+import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
   return (
     <div
       className="
-        border-[1px]
+        py-2
         w-full
         md:w-auto
-        py-2
-        rounded-full
         shadow-sm
-        hover:shadow-md
         transition
+        border-[1px]
+        rounded-full
         cursor-pointer
+        hover:shadow-md
       "
     >
       <div
@@ -25,26 +27,50 @@ const Search = () => {
       >
         <div
           className="
+            px-6
             text-sm
             font-semibold
-            px-6
           "
         >
           Anywere
         </div>
         <div
           className="
-          hidden
-          sm:block
-          text-sm
-          font-semibold
-          px-6
-          border-x-[1px]
-          flex-1
-          text-center
+            px-6
+            hidden
+            flex-1
+            text-sm
+            sm:block
+            text-center
+            border-x-[1px]
+            font-semibold
           "
         >
           Any Week
+        </div>
+        <div
+          className="
+            flex
+            pl-6
+            pr-2
+            gap-3
+            text-sm
+            flex-row
+            items-center
+            text-gray-600
+          "
+        >
+          <div className="hidden sm:block">Add Guests</div>
+          <div
+            className="
+              p-2
+              text-white
+              bg-rose-500
+              rounded-full
+            "
+          >
+            <BiSearch size={18} />
+          </div>
         </div>
       </div>
     </div>
