301fcecbb2e64f13d284e8439d0b0618d9291112
cqxg <ihar.o.ivaniuk@gmail.com>
2023-05-19 23:45:33 +0200

    feat: add google auth

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -77,7 +77,7 @@ const RegisterModal = () => {
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => {}}
+        onClick={() => signIn("google")}
       />
       <Button
         outline
