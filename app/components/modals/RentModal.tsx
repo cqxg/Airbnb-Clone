308a8ca02e61f24170d32a1a8381a97955109c37
cqxg <ihar.o.ivaniuk@gmail.com>
2023-05-27 23:12:54 +0200

    feat: add rent modal submit handler

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import axios from "axios";
+import { toast } from "react-hot-toast";
 import { useMemo, useState } from "react";
-import { FieldValues, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
 import Map from "../Map";
 import Modal from "./Modal";
@@ -24,6 +27,7 @@ enum STEPS {
 }
 
 const RentModal = () => {
+  const router = useRouter();
   const rentModal = useRentModal();
 
   const [step, setStep] = useState(STEPS.CATEGORY);
@@ -73,6 +77,30 @@ const RentModal = () => {
       bathroomCount: 1,
     },
   });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+
+    setIsLoading(true);
+
+    axios
+      .post("/api/listings", data)
+      .then(() => {
+        toast.success("Listing created!");
+        router.refresh();
+        reset();
+        setStep(STEPS.CATEGORY);
+        rentModal.onClose();
+      })
+      .catch(() => {
+        toast.error("Something went wrong.");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   const location = watch("location");
   const category = watch("category");
@@ -232,12 +260,12 @@ const RentModal = () => {
 
   return (
     <Modal
-      onSubmit={onNext}
       body={bodyContent}
       title="Airbnb your home!"
       isOpen={rentModal.isOpen}
       actionLabel={actionLabel}
       onClose={rentModal.onClose}
+      onSubmit={handleSubmit(onSubmit)}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
     />
