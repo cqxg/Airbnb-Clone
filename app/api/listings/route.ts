231c05503da0c41dda32bad59ae55be01cf79361
cqxg <ihar.o.ivaniuk@gmail.com>
2023-05-28 12:22:14 +0200

    feat: add create listing api

---
+++ app/api/listings/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import prisma from "@/app/libs/prismadb";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.error();
+  }
+
+  const body = await request.json();
+  const {
+    title,
+    price,
+    imageSrc,
+    category,
+    location,
+    roomCount,
+    guestCount,
+    description,
+    bathroomCount,
+  } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
+    }
+  });
+
+  const listing = await prisma.listing.create({
+    data: {
+      title,
+      imageSrc,
+      category,
+      roomCount,
+      guestCount,
+      description,
+      bathroomCount,
+      locationValue: location.value,
+      price: parseInt(price, 10),
+      userId: currentUser.id,
+    },
+  });
+
+  return NextResponse.json(listing);
+}
