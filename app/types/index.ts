42fd4c622b737fd5d46f6227da59bb6fabb1c41e
cqxg <ihar.o.ivaniuk@gmail.com>
2023-06-06 11:45:10 +0200

    feat: add SafeReservation type

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,5 +1,15 @@
-import { Listing } from "@prisma/client";
+import { Listing, Reservation } from "@prisma/client";
 
 export type SafeListing = Omit<Listing, "createdAt"> & {
   createdAt: string;
+};
+
+export type SafeReservation = Omit<
+  Reservation,
+  "createdAt" | "startDate" | "endDate" | "listing"
+> & {
+  endDate: string;
+  createdAt: string;
+  startDate: string;
+  listing: SafeListing;
 };
